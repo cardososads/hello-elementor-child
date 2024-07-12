0740b7156b66682bebbdde816284589fe3e761f2
Wesley Alves Cardoso <66920722+cardososads@users.noreply.github.com>
2024-07-12 14:55:06 -0400

    Show other players

--- functions.php
+++ functions.php
@@ -12,31 +12,34 @@ require get_stylesheet_directory() . '/inc/class-numerology-calculator.php';
 // Hook para processar o envio dos formulários
 add_action('elementor_pro/forms/new_record', 'process_elementor_form_submission', 10, 2);
 
-add_action('elementor_pro/forms/new_record', 'process_elementor_form_submission', 10, 2);
-
 function process_elementor_form_submission($record, $handler) {
-    // Verifique qual formulário foi enviado
     $form_name = $record->get_form_settings('form_name');
+    $fields = [];
+    foreach ($record->get('fields') as $field) {
+        $fields[$field['name']] = $field['value'];
+    }
 
-    // Obtenha os dados do formulário
-    $fields = array_map(function($field) {
-        return $field['value'];
-    }, $record->get('fields'));
-
-    // Instancia a classe de cálculo
     $calculator = new NumerologyCalculator();
 
-    // Processa os dados conforme o formulário enviado
     switch ($form_name) {
         case 'Form1':
-            $fields['destiny_number'] = $calculator->calculateDestinyNumber($fields['birth_date']);
+            if (isset($fields['birth_date'])) {
+                $fields['destiny_number'] = $calculator->calculateDestinyNumber($fields['birth_date']);
+            }
             break;
         case 'Form2':
-            $fields['expression_number'] = $calculator->calculateExpressionNumber($fields['full_name']);
+            if (isset($fields['full_name'])) {
+                $fields['expression_number'] = $calculator->calculateExpressionNumber($fields['full_name']);
+                $fields['motivation_number'] = $calculator->calculateMotivationNumber($fields['full_name']);
+            }
+            break;
+        case 'Form3':
+            if (isset($fields['full_name'])) {
+                $fields['motivation_number'] = $calculator->calculateMotivationNumber($fields['full_name']);
+            }
             break;
     }
 
-    // Armazena os dados do formulário usando transients para acesso global
     set_transient("form{$form_name}_submission_data", $fields, HOUR_IN_SECONDS);
 }
 
