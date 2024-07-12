e506020c0618714d18c2f84b2f3d6e3f13d7fa72
Wesley Alves Cardoso <66920722+cardososads@users.noreply.github.com>
2024-07-12 16:12:23 -0400

    Show other players

--- functions.php
+++ functions.php
@@ -105,7 +105,7 @@ function return_acf_introduction_options($form_name = 'Form1')
                 $subtitles[] = [];
             }
         }
-
+        var_dump($data);
         foreach ($nums_destino as $option) {
             if ($data['destiny_number'] == $option['numero_destino_']) {
                 $audio_files[] = $option['audio_destino_'];
