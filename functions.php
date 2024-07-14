2023698ea63f20a5ccdc9f3b6b7cc9c30ffc0fbb
Wesley Cardoso <cardososads@github.com>
2024-07-14 06:59:04 -0400

    Session Modifications

--- functions.php
+++ functions.php
@@ -134,18 +134,20 @@ function return_acf_introduction_options($form_name = 'Form1')
         var_dump($expression_number);
         echo '</pre>';
         foreach ($nums_expressao as $option) {
-            $audio_files[] = $option['audio_expressao_'];
-            $legenda_json = $option['legenda_expressao_'];
-            echo '<pre>';
-            var_dump($option);
-            echo '</pre>';            // Correção do JSON: adicionar aspas duplas corretamente
-            $legenda_json = preg_replace('/(\w+):/i', '"$1":', $legenda_json);
-            $legenda = json_decode($legenda_json, true);
+            if($expression_number == $option['numero_expressao_'] && $option['genero_expressao_'] == $gender){
+                $audio_files[] = $option['audio_expressao_'];
+                $legenda_json = $option['legenda_expressao_'];
+                echo '<pre>';
+                var_dump($option);
+                echo '</pre>';            // Correção do JSON: adicionar aspas duplas corretamente
+                $legenda_json = preg_replace('/(\w+):/i', '"$1":', $legenda_json);
+                $legenda = json_decode($legenda_json, true);
 
-            if (json_last_error() === JSON_ERROR_NONE) {
-                $subtitles[] = $legenda;
-            } else {
-                $subtitles[] = [];
+                if (json_last_error() === JSON_ERROR_NONE) {
+                    $subtitles[] = $legenda;
+                } else {
+                    $subtitles[] = [];
+                }
             }
         }
 
