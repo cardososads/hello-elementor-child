f7f7be8088ca34fe4a2ab59fd81b28269ba3c766
Wesley Cardoso <cardososads@github.com>
2024-07-14 07:56:43 -0400

    Session Modifications

--- functions.php
+++ functions.php
@@ -149,7 +149,7 @@ function return_acf_introduction_options($form_name = 'Form1')
     } else if ($form_name === 'Form3') {
         $calculator = new NumerologyCalculator();
         $motivation_number = $calculator->calculateMotivationNumber($data['full_name']); // Supondo que 'motivation_number' está disponível nos dados do formulário
-        var_dump($data, $motivation_number);
+        var_dump($data, $motivation_number, $nums_motivacao);
         $relationship_status = $data['relationship_status']; // Supondo que 'relationship_status' está disponível nos dados do formulário
         foreach ($nums_motivacao as $option) {
             if ($motivation_number == $option['numero_motivacao_'] && $option['estado_civil_motivacao_'] == $relationship_status) {
