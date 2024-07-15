48157a713caa6ff889cf2f5140213fa7d5282fcb
Wesley Cardoso <cardososads@github.com>
2024-07-15 01:43:53 -0400

    Functions_JS

--- functions.php
+++ functions.php
@@ -1,17 +1,17 @@
 <?php
 
-function hello_elementor_child_enqueue_styles()
-{
-    // Enfileira o estilo principal do tema pai
-    wp_enqueue_style('hello-elementor-style', get_template_directory_uri() . '/style.css');
+// function hello_elementor_child_enqueue_styles()
+// {
+//     // Enfileira o estilo principal do tema pai
+//     wp_enqueue_style('hello-elementor-style', get_template_directory_uri() . '/style.css');
 
-    // Enfileira o estilo do tema filho, que depende do estilo do tema pai
-    wp_enqueue_style('hello-elementor-child-style', get_stylesheet_directory_uri() . '/style.css', array('hello-elementor-style'), '1.0');
+//     // Enfileira o estilo do tema filho, que depende do estilo do tema pai
+//     wp_enqueue_style('hello-elementor-child-style', get_stylesheet_directory_uri() . '/style.css', array('hello-elementor-style'), '1.0');
 
-    // Enfileira o script customizado, que depende do jQuery e será carregado no footer
-    wp_enqueue_script('functions_js', get_stylesheet_directory_uri() . '/js/functions.js', array('jquery'), '1.0', true);
-}
-add_action('wp_enqueue_scripts', 'hello_elementor_child_enqueue_styles');
+//     // Enfileira o script customizado, que depende do jQuery e será carregado no footer
+//     wp_enqueue_script('functions_js', get_stylesheet_directory_uri() . '/js/functions.js', array('jquery'), '1.0', true);
+// }
+// add_action('wp_enqueue_scripts', 'hello_elementor_child_enqueue_styles');
 
 
 require get_stylesheet_directory() . '/inc/class-acf-options.php';
