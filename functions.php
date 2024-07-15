c170a88711b7b33dce1e17e13b2fc688dcb100e6
Wesley Cardoso <cardososads@github.com>
2024-07-15 06:17:51 -0400

    Functions_JS

--- functions.php
+++ functions.php
@@ -19,29 +19,28 @@ function script_form()
 {
 ?>
     <script type="text/javascript">
-        document.addEventListener('DOMContentLoaded', function() {
-            var painelExecucao = document.querySelector('.painel_execucao');
-            var secForm = document.querySelector('.sec_form');
-            var playersContainer = document.querySelector('.players');
-            var numeroDestinoStyle = document.getElementById('numero_destino_style');
-            var sol = document.getElementById('sol');
-            var players = playersContainer ? playersContainer.querySelectorAll('audio') : [];
+        jQuery(document).ready(function($) {
+            var painelExecucao = $('.painel_execucao');
+            var secForm = $('.sec_form');
+            var playersContainer = $('.players');
+            var numeroDestinoStyle = $('#numero_destino_style');
+            var sol = $('#sol');
+            var players = playersContainer ? playersContainer.find('audio') : [];
             var playersFinished = 0;
 
             // Oculta sec_form inicialmente
             if (secForm) {
-                secForm.style.display = 'none';
+                secForm.hide();
             }
             // Oculta numero_destino_style inicialmente
             if (numeroDestinoStyle) {
-                numeroDestinoStyle.style.display = 'none';
-                numeroDestinoStyle.style.width = '0';
+                numeroDestinoStyle.hide().css('width', '0');
             }
 
             // Exibe painel_execucao com fade-in no carregamento da página
             if (painelExecucao) {
                 setTimeout(function() {
-                    jQuery(painelExecucao).fadeIn(1000);
+                    painelExecucao.fadeIn(1000);
                 }, 2000); // 2 segundos de atraso
             }
 
@@ -51,9 +50,9 @@ function script_form()
                     // Todos os players terminaram
                     if (painelExecucao && secForm) {
                         setTimeout(function() {
-                            jQuery(painelExecucao).fadeOut(1000, function() {
+                            painelExecucao.fadeOut(1000, function() {
                                 setTimeout(function() {
-                                    jQuery(secForm).fadeIn(1000);
+                                    secForm.fadeIn(1000);
                                 }, 1000); // 1 segundo de atraso após fade-out
                             });
                         }, 1000); // 1 segundo de atraso antes do fade-out
@@ -62,26 +61,26 @@ function script_form()
             }
 
             // Adiciona eventos de término e início aos players
-            players.forEach(function(player, index) {
-                player.addEventListener('ended', function() {
+            players.each(function(index, player) {
+                $(player).on('ended', function() {
                     playersFinished++;
                     checkPlayers();
                 });
                 if (index === players.length - 1) {
-                    player.addEventListener('play', function() {
+                    $(player).on('play', function() {
                         if (numeroDestinoStyle) {
-                            numeroDestinoStyle.style.display = 'block';
+                            numeroDestinoStyle.show();
                             // Adiciona um pequeno atraso antes de adicionar a classe para garantir que a transição seja aplicada
                             setTimeout(function() {
                                 console.log('Adicionando classe show ao numeroDestinoStyle');
-                                numeroDestinoStyle.classList.add('show');
+                                numeroDestinoStyle.addClass('show');
                             }, 50); // Atraso de 50ms
                         }
                         if (sol) {
                             // Adiciona um pequeno atraso antes de adicionar a classe para garantir que a transição seja aplicada
                             setTimeout(function() {
                                 console.log('Adicionando classe show ao sol');
-                                sol.classList.add('show');
+                                sol.addClass('show');
                             }, 50); // Atraso de 50ms
                         }
                     });
@@ -134,45 +133,33 @@ function return_acf_introduction_options($form_name = 'Form1')
     $nums_motivacao = ACFOptions::get_field('acf_numeros_de_motivacao');
 ?>
     <script type="text/javascript">
-        var form1_data = JSON.parse(localStorage.getItem('Form1_data') || '{}');
-        var form2_data = JSON.parse(localStorage.getItem('Form2_data') || '{}');
-        var form3_data = JSON.parse(localStorage.getItem('Form3_data') || '{}');
-        var form_data = {};
+        jQuery(document).ready(function($) {
+            var form1_data = JSON.parse(localStorage.getItem('Form1_data') || '{}');
+            var form2_data = JSON.parse(localStorage.getItem('Form2_data') || '{}');
+            var form3_data = JSON.parse(localStorage.getItem('Form3_data') || '{}');
+            var form_data = {};
 
-        switch ('<?= $form_name ?>') {
-            case 'Form1':
-                form_data = form1_data;
-                break;
-            case 'Form2':
-                form_data = Object.assign({}, form1_data, form2_data);
-                break;
-            case 'Form3':
-                form_data = Object.assign({}, form1_data, form2_data, form3_data);
-                break;
-        }
+            switch ('<?= $form_name ?>') {
+                case 'Form1':
+                    form_data = form1_data;
+                    break;
+                case 'Form2':
+                    form_data = $.extend({}, form1_data, form2_data);
+                    break;
+                case 'Form3':
+                    form_data = $.extend({}, form1_data, form2_data, form3_data);
+                    break;
+            }
 
-        localStorage.setItem('<?= $form_name ?>_final_data', JSON.stringify(form_data));
+            localStorage.setItem('<?= $form_name ?>_final_data', JSON.stringify(form_data));
 
-        var audio_files = [];
-        var subtitles = [];
+            var audio_files = [];
+            var subtitles = [];
 
-        <?php if ($form_name === 'Form1') : ?>
-            <?php foreach ($intros as $option) : ?>
-                audio_files.push('<?= $option['audio_de_introducao_'] ?>');
-                var legenda_json = '<?= addslashes($option['legenda_de_introducao_']) ?>';
-                legenda_json = legenda_json.replace(/(\w+):/g, '"$1":');
-                var legenda = JSON.parse(legenda_json);
-
-                if (legenda) {
-                    subtitles.push(legenda);
-                } else {
-                    subtitles.push([]);
-                }
-            <?php endforeach; ?>
-            <?php foreach ($nums_destino as $option) : ?>
-                if (form_data['destiny_number'] == <?= $option['numero_destino_'] ?>) {
-                    audio_files.push('<?= $option['audio_destino_'] ?>');
-                    var legenda_json = '<?= addslashes($option['legenda_destino_']) ?>';
+            <?php if ($form_name === 'Form1') : ?>
+                <?php foreach ($intros as $option) : ?>
+                    audio_files.push('<?= $option['audio_de_introducao_'] ?>');
+                    var legenda_json = '<?= addslashes($option['legenda_de_introducao_']) ?>';
                     legenda_json = legenda_json.replace(/(\w+):/g, '"$1":');
                     var legenda = JSON.parse(legenda_json);
 
@@ -181,147 +168,113 @@ function return_acf_introduction_options($form_name = 'Form1')
                     } else {
                         subtitles.push([]);
                     }
-                }
-            <?php endforeach; ?>
-        <?php elseif ($form_name === 'Form2') : ?>
-            var gender = form_data['gender'];
-            var expression_number = form_data['expression_number'];
-            <?php foreach ($nums_expressao as $option) : ?>
-                if (expression_number == <?= $option['numero_expressao_'] ?> && gender == '<?= $option['genero_expressao_'] ?>') {
-                    audio_files.push('<?= $option['audio_expressao_'] ?>');
-                    var legenda_json = '<?= addslashes($option['legenda_expressao_']) ?>';
-                    legenda_json = legenda_json.replace(/(\w+):/g, '"$1":');
-                    var legenda = JSON.parse(legenda_json);
+                <?php endforeach; ?>
+                <?php foreach ($nums_destino as $option) : ?>
+                    if (form_data['destiny_number'] == <?= $option['numero_destino_'] ?>) {
+                        audio_files.push('<?= $option['audio_destino_'] ?>');
+                        var legenda_json = '<?= addslashes($option['legenda_destino_']) ?>';
+                        legenda_json = legenda_json.replace(/(\w+):/g, '"$1":');
+                        var legenda = JSON.parse(legenda_json);
 
-                    if (legenda) {
-                        subtitles.push(legenda);
-                    } else {
-                        subtitles.push([]);
+                        if (legenda) {
+                            subtitles.push(legenda);
+                        } else {
+                            subtitles.push([]);
+                        }
                     }
-                }
-            <?php endforeach; ?>
-        <?php elseif ($form_name === 'Form3') : ?>
-            var motivation_number = form_data['motivation_number'];
-            var relationship_status = form_data['marital_status'];
-            <?php foreach ($nums_motivacao as $option) : ?>
-                if (motivation_number == <?= $option['numero_motivacao_'] ?> && relationship_status == '<?= $option['estado_civil_motivacao_'] ?>') {
-                    audio_files.push('<?= $option['audio_motivacao_'] ?>');
-                    var legenda_json = '<?= addslashes($option['legenda_motivacao_']) ?>';
-                    legenda_json = legenda_json.replace(/(\w+):/g, '"$1":');
-                    var legenda = JSON.parse(legenda_json);
+                <?php endforeach; ?>
+            <?php elseif ($form_name === 'Form2') : ?>
+                var gender = form_data['gender'];
+                var expression_number = form_data['expression_number'];
+                <?php foreach ($nums_expressao as $option) : ?>
+                    if (expression_number == <?= $option['numero_expressao_'] ?> && gender == '<?= $option['genero_expressao_'] ?>') {
+                        audio_files.push('<?= $option['audio_expressao_'] ?>');
+                        var legenda_json = '<?= addslashes($option['legenda_expressao_']) ?>';
+                        legenda_json = legenda_json.replace(/(\w+):/g, '"$1":');
+                        var legenda = JSON.parse(legenda_json);
 
-                    if (legenda) {
-                        subtitles.push(legenda);
-                    } else {
-                        subtitles.push([]);
+                        if (legenda) {
+                            subtitles.push(legenda);
+                        } else {
+                            subtitles.push([]);
+                        }
                     }
-                }
-            <?php endforeach; ?>
-        <?php endif; ?>
-    </script>
-    <?php
+                <?php endforeach; ?>
+            <?php elseif ($form_name === 'Form3') : ?>
+                var motivation_number = form_data['motivation_number'];
+                var relationship_status = form_data['marital_status'];
+                <?php foreach ($nums_motivacao as $option) : ?>
+                    if (motivation_number == <?= $option['numero_motivacao_'] ?> && relationship_status == '<?= $option['estado_civil_motivacao_'] ?>') {
+                        audio_files.push('<?= $option['audio_motivacao_'] ?>');
+                        var legenda_json = '<?= addslashes($option['legenda_motivacao_']) ?>';
+                        legenda_json = legenda_json.replace(/(\w+):/g, '"$1":');
+                        var legenda = JSON.parse(legenda_json);
 
-    foreach ($audio_files as $index => $audio_src) {
-    ?>
-        <audio id="audio_player_<?= $index ?>" src="<?= $audio_src ?>" controls <?= $index > 0 ? 'style="display:none;"' : '' ?>></audio>
-        <div id="legenda_<?= $index ?>" class="legenda" style="display: none;"></div>
-    <?php
-    }
-    ?>
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            const audioPlayers = document.querySelectorAll('audio');
-            const subtitles = <?php echo json_encode($subtitles); ?>;
-            const legendaDivs = document.querySelectorAll('.legenda');
+                        if (legenda) {
+                            subtitles.push(legenda);
+                        } else {
+                            subtitles.push([]);
+                        }
+                    }
+                <?php endforeach; ?>
+            <?php endif; ?>
 
-            function updateLegenda(index, currentTime) {
-                const legendasParaAudio = subtitles[index];
-                const legendaDiv = legendaDivs[index];
-                let displayed = false;
+            // Renderizar os players de áudio e legendas
+            var audioContainer = $('#audio_container');
+            for (var i = 0; i < audio_files.length; i++) {
+                var audioPlayer = $('<audio>', {
+                    id: 'audio_player_' + i,
+                    src: audio_files[i],
+                    controls: true,
+                    style: i > 0 ? 'display:none;' : ''
+                });
+                var legendaDiv = $('<div>', {
+                    id: 'legenda_' + i,
+                    class: 'legenda',
+                    style: 'display: none;'
+                });
+                audioContainer.append(audioPlayer).append(legendaDiv);
+            }
 
-                for (let i = 0; i < legendasParaAudio.length; i++) {
-                    if (legendasParaAudio[i].time <= currentTime && (!legendasParaAudio[i + 1] || legendasParaAudio[i + 1].time > currentTime)) {
-                        legendaDiv.innerText = legendasParaAudio[i].text;
-                        legendaDiv.style.display = 'block';
+            // Atualizar as legendas durante a reprodução do áudio
+            $('audio').on('timeupdate', function() {
+                var index = $(this).attr('id').split('_')[2];
+                var currentTime = this.currentTime;
+                var legendasParaAudio = subtitles[index];
+                var legendaDiv = $('#legenda_' + index);
+                var displayed = false;
+
+                for (var j = 0; j < legendasParaAudio.length; j++) {
+                    if (legendasParaAudio[j].time <= currentTime && (!legendasParaAudio[j + 1] || legendasParaAudio[j + 1].time > currentTime)) {
+                        legendaDiv.text(legendasParaAudio[j].text).show();
                         displayed = true;
                         break;
                     }
                 }
 
                 if (!displayed) {
-                    legendaDiv.style.display = 'none';
+                    legendaDiv.hide();
                 }
-            }
-
-            audioPlayers.forEach((audio, index) => {
-                audio.addEventListener('play', function() {
-                    legendaDivs.forEach(div => div.style.display = 'none');
-                });
-
-                audio.addEventListener('timeupdate', function() {
-                    updateLegenda(index, audio.currentTime);
-                });
-
-                audio.addEventListener('ended', function() {
-                    audio.style.display = 'none';
-                    legendaDivs[index].style.display = 'none';
-                    const nextAudio = audioPlayers[index + 1];
-                    if (nextAudio) {
-                        nextAudio.style.display = 'block';
-                        nextAudio.play();
-                    }
-                });
             });
 
-            if (audioPlayers.length > 0) {
-                audioPlayers[0].play();
+            // Iniciar o próximo áudio ao término do atual
+            $('audio').on('ended', function() {
+                var index = $(this).attr('id').split('_')[2];
+                $(this).hide();
+                $('#legenda_' + index).hide();
+                var nextAudio = $('#audio_player_' + (parseInt(index) + 1));
+                if (nextAudio.length) {
+                    nextAudio.show().trigger('play');
+                }
+            });
+
+            // Iniciar o primeiro áudio automaticamente
+            if ($('audio').length > 0) {
+                $('#audio_player_0').trigger('play');
             }
         });
     </script>
-    <style>
-        audio {
-            width: 80%;
-            height: 50px;
-            margin: 0 auto;
-            display: block;
-        }
-
-        .legenda {
-            margin-top: 10px;
-            font-size: 1.3em;
-            /* Tamanho de fonte equivalente a h2 */
-            color: white;
-            text-align: center;
-        }
-
-        .num_dest {
-            color: white;
-        }
-
-        #numero_destino_style {
-            display: none;
-            /* Escondido inicialmente */
-            width: 0;
-            transition: width 1s ease-in-out;
-            /* Transição suave para a largura */
-        }
-
-        #numero_destino_style.show {
-            display: flex !important;
-            width: 40% !important;
-        }
-
-        #sol {
-            width: 50% !important;
-            /* Largura inicial maior */
-            transition: width 1s ease-in-out;
-            /* Transição suave para a largura */
-        }
-
-        #sol.show {
-            width: 15% !important;
-        }
-    </style>
+    <div id="audio_container"></div>
 <?php
 }
 
@@ -342,10 +295,13 @@ function get_destiny_number()
 {
 ?>
     <script type="text/javascript">
-        var form1_data = JSON.parse(localStorage.getItem('Form1_final_data') || '{}');
-        var destiny_number = form1_data['destiny_number'] || 'No destiny number found';
-        document.write('<p class="num_destino">' + destiny_number + '</p>');
+        jQuery(document).ready(function($) {
+            var form1_data = JSON.parse(localStorage.getItem('Form1_final_data') || '{}');
+            var destiny_number = form1_data['destiny_number'] || 'No destiny number found';
+            $('.num_destino').text(destiny_number);
+        });
     </script>
+    <p class="num_destino"></p>
 <?php
 }
 
