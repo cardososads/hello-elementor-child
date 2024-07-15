8dcc19d1089cc42b186830a55f72b07e4aeb17cc
Wesley Cardoso <cardososads@github.com>
2024-07-15 01:23:31 -0400

    Functions_JS

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ function hello_elementor_child_enqueue_styles()
 {
     wp_enqueue_style('hello-elementor-style', get_template_directory_uri() . 'style.css');
     wp_enqueue_style('hello-elementor-child-style', get_stylesheet_directory_uri() . 'style.css', array('hello-elementor-style'));
-    wp_enqueue_script('functions_js', get_stylesheet_directory_uri() . '/functions.js');
+    wp_enqueue_script('functions_js', get_stylesheet_directory_uri() . '/functions.js', array(), null);
 }
 add_action('wp_enqueue_scripts', 'hello_elementor_child_enqueue_styles');
 
